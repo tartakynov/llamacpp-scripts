3bcefd7eacf52df609fafee7c4dc443d9f65a3a7
Artem Tartakynov <artem.tad@gmail.com>
2023-11-16 21:09:57 -0500

    add script to run the models

--- server.sh
+++ server.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [7b|13b|X]"
+    echo "  7b: Run server with path of file A in models folder"
+    echo "  13b: Run server with path of file B in models folder"
+}
+
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+case $1 in
+    7b)
+        echo "Running server with File A..."
+        MODEL="models/llama-2-7b-chat.Q4_0.gguf"
+        ;;
+    13b)
+        echo "Running server with File B..."
+        MODEL="models/llama-2-13b-chat.Q4_0.gguf"
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        usage
+        exit 1
+        ;;
+esac
+
 LLAMA_CPP_FOLDER=~/src/llama.cpp
-MODEL=models/llama-2-7b-chat.Q4_0.gguf
 
 open http://localhost:8080
 
@@ -9,3 +37,6 @@ open http://localhost:8080
   -m "$MODEL" \
   --ctx-size 2048 \
   --threads 8
+
+
+
