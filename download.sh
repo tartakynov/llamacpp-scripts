ad61749b784d77d23f08eda3fb6fd76b23bcba94
Artem Tartakynov <artem.tad@gmail.com>
2023-11-16 21:51:50 -0500

    add ability to run BakLLaVa-1 model

--- download.sh
+++ download.sh
@@ -2,9 +2,10 @@
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 [7b|13b]"
+    echo "Usage: $0 [7b|13b|bakllava]"
     echo "  7b: Download llama2 7B model"
     echo "  13b: Download llama2 13B model"
+    echo "  bakllava: Download BakLLaVa-1 model"
 }
 
 # Check if no arguments were provided
@@ -16,12 +17,20 @@ fi
 # Depending on the argument, download the appropriate file
 case $1 in
     7b)
-        echo "Downloading llama2 7B..."
-        curl -L -o models/llama-2-7b-chat.Q4_K_M.gguf https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf?download=true
+        echo "Downloading LLama 7B..."
+        mkdir -p models/llama-2/
+        curl -L -o models/llama-2/llama-2-7b-chat.Q4_K_M.gguf https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf?download=true
         ;;
     13b)
-        echo "Downloading llama2 13B..."
-        curl -L -o models/llama-2-13b-chat.Q4_K_M.gguf https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_K_M.gguf?download=true
+        echo "Downloading LLama 13B..."
+        mkdir -p models/llama-2/
+        curl -L -o models/llama-2/llama-2-13b-chat.Q4_K_M.gguf https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_K_M.gguf?download=true
+        ;;
+    bakllava)
+        echo "Downloading BakLLaVa-1"
+        mkdir -p models/bakllava/
+        curl -L -o models/bakllava/ggml-model-q4_k.gguf https://huggingface.co/mys/ggml_bakllava-1/resolve/main/ggml-model-q4_k.gguf?download=true
+        curl -L -o models/bakllava/mmproj-model-f16.gguf https://huggingface.co/mys/ggml_bakllava-1/resolve/main/mmproj-model-f16.gguf?download=true
         ;;
     *)
         echo "Invalid argument: $1"
--- server.sh
+++ server.sh
@@ -5,8 +5,9 @@ LLAMA_CPP_FOLDER=~/src/llama.cpp
 # Function to display usage information
 usage() {
     echo "Usage: $0 [7b|13b|X]"
-    echo "  7b: Run server with path of file A in models folder"
-    echo "  13b: Run server with path of file B in models folder"
+    echo "  7b: Run server with Llama 2 7B model"
+    echo "  13b: Run server with Llama 2 13B model"
+    echo "  bakllava: Run server with BakLLaVa-1 model"
 }
 
 # Check if no arguments were provided
@@ -17,12 +18,16 @@ fi
 
 case $1 in
     7b)
-        echo "Running server with File A..."
-        MODEL="models/llama-2-7b-chat.Q4_K_M.gguf"
+        echo "Running server with LLama 2 7B..."
+        MODEL_ARGS="-m models/llama-2/llama-2-7b-chat.Q4_K_M.gguf"
         ;;
     13b)
-        echo "Running server with File B..."
-        MODEL="models/llama-2-13b-chat.Q4_K_M.gguf"
+        echo "Running server with LLama 2 13B..."
+        MODEL_ARGS="-m models/llama-2/llama-2-13b-chat.Q4_K_M.gguf"
+        ;;
+    bakllava)
+        echo "Running server with BakLLaVa-1..."
+        MODEL_ARGS="-m models/bakllava/ggml-model-q4_k.gguf --mmproj models/bakllava/mmproj-model-f16.gguf"
         ;;
     *)
         echo "Invalid argument: $1"
@@ -34,7 +39,7 @@ esac
 open http://localhost:8080
 
 "$LLAMA_CPP_FOLDER"/server \
-  -m "$MODEL" \
+  $MODEL_ARGS \
   --ctx-size 2048 \
   --threads 8
 
